9edd08e4e0f794f424af45c2fe1619f0b7b48830
ethantanen <e_tanen@coloradocollege.edu>
2018-06-12 18:34:54 -0400

    prettied up cli, changed naming conventions for readability and standardization

--- photo_db_cli/db_logic.js
+++ photo_db_cli/db_logic.js
@@ -1,41 +1,52 @@
-AWS = require("aws-sdk")
-express = require("express")
-fs = require("fs")
+const aws = require("aws-sdk")
+const express = require("express")
+const fs = require("fs")
 
 // Create express instance
 var app = express()
 
 // Create S3 client, set credentials in 'credentials' file
-//apiVersion option keeps api version consistent accross platforms
-var s3 = new AWS.S3({
-  apiVersion: '2006-03-01',
+var s3 = new aws.S3({
+  apiVersion: '2006-03-01', // specifying version keeps api consistent accross platforms
 })
 
-// Suffix to make bucket names unique
+// Suffix appended to bucket name to make bucket names unique
 const BUCKET_SUFFIX = "-ventera-summer-2018"
 
-// Create new bucket with users name
-function add_bucket(name) {
-  // Return promise with data on location of bucket
+
+/*
+ * All functions are of a similar form. Each function
+ * returns a promise. Each function defines a parameters object
+ * that's passed to the s3 api. The paramter list insantiation
+ * is followed by a single call to the s3 api. Success and failure
+ * print outs are left to the calling function, so as to generalize
+ * this files usagpe capabilities.
+ */
+
+// Create new bucket with the given name
+function add_bucket(bucket_name) {
   return new Promise((resolve, reject) => {
-    s3.createBucket({
-      Bucket: name + BUCKET_SUFFIX
-    }, (err, data) => {
+
+    var params = {
+      Bucket: bucket_name + BUCKET_SUFFIX
+    }
+
+    s3.createBucket(params, (err, data) => {
       if (err) return reject(err)
       return resolve(data)
     })
+
   })
 }
 
-// Add a single image ot the specified users bucket
-function add_object(name, filename, file) {
+// Add new image to the specified bucket
+function add_object(bucket_name, object_name, object) {
   return new Promise((resolve, reject) => {
-    console.log(name,filename,file)
-    // Information on object to be uploaded
+
     var params = {
-      Body: file,
-      Bucket: name + BUCKET_SUFFIX,
-      Key: filename,
+      Body: object,
+      Bucket: bucket_name + BUCKET_SUFFIX,
+      Key: object_name,
     };
 
     // Upload object
@@ -43,87 +54,94 @@ function add_object(name, filename, file) {
       if (err) return reject(err)
       return resolve(data)
     })
+
   })
 }
 
-// Get list of all users, which is list of all buckets
-function get_buckets() {
+// Get list of all buckets
+function list_buckets() {
   return new Promise((resolve, reject) => {
-    s3.listBuckets({}, (err,data) => {
-      if(err) return reject(err)
+
+    s3.listBuckets({}, (err, data) => {
+      if (err) return reject(err)
       return resolve(data)
     })
+
   })
 }
 
-// Get list of users photos
-function get_objects(name, num) {
+// Get list of buckets objects
+function list_objects(bucket_name, max_objects) {
   return new Promise((resolve, reject) => {
 
-    //Bucket to retrieve from and max number of photos to return
     var params = {
-      Bucket: name + BUCKET_SUFFIX,
-      MaxKeys: num,
+      Bucket: bucket_name + BUCKET_SUFFIX,
+      MaxKeys: max_objects,
     }
 
-    s3.listObjects(params, (err,data) => {
-      if(err) return reject(err)
+    s3.listObjects(params, (err, data) => {
+      if (err) return reject(err)
       return resolve(data)
     })
+
   })
 }
 
 // Get a document from the database
-function get_object(username,filename) {
-  return new Promise((resolve,reject) => {
+function get_object(bucket_name, object_name) {
+  return new Promise((resolve, reject) => {
 
     var params = {
-      Bucket: username + BUCKET_SUFFIX,
-      Key: filename
+      Bucket: bucket_name + BUCKET_SUFFIX,
+      Key: object_name
     }
-    console.log(username, filename)
-    s3.getObject(params).createReadStream().pipe(fs.createWriteStream("./"+filename))
+
+    s3.getObject(params)
+      .createReadStream()
+      .pipe(fs.createWriteStream("./" + object_name))
+
   })
 }
 
 // Delete a users
-function delete_bucket(name) {
-  return new Promise((resolve,reject) => {
+function delete_bucket(bucket_name) {
+  return new Promise((resolve, reject) => {
 
     var params = {
-      Bucket: name + BUCKET_SUFFIX,
+      Bucket: bucket_name + BUCKET_SUFFIX,
     }
 
     s3.deleteBucket(params, (err, data) => {
-      if(err) return reject(err)
+      if (err) return reject(err)
       return resolve(data)
     })
   })
 }
 
 // Delete a photo with the photo's key as identification
-function delete_object(name, photo_key) {
+function delete_object(bucket_name, object_key) {
   return new Promise((resolve, reject) => {
 
     var params = {
-      Bucket: name + BUCKET_SUFFIX,
-      Key: photo_key,
+      Bucket: bucket_name + BUCKET_SUFFIX,
+      Key: object_key,
     }
 
-    s3.deleteObject(params, (err,data) => {
-      if(err) return reject(err)
+    s3.deleteObject(params, (err, data) => {
+      if (err) return reject(err)
       return resolve(data)
     })
+
   })
 }
 
-
+// Allows other js files to require this file
 module.exports = {
-  "add_bucket":add_bucket,
-  "add_object":add_object,
-  "get_buckets":get_buckets,
-  "get_objects":get_objects,
-  "delete_bucket":delete_bucket,
-  "delete_object":delete_object,
-  "get_object":get_object
+  "add_bucket": add_bucket,
+  "add_object": add_object,
+  "list_buckets": list_buckets,
+  "list_objects": list_objects,
+  "delete_bucket": delete_bucket,
+  "delete_object": delete_object,
+  "get_object": get_object
 }
--- photo_db_cli/photo_db_cli.js
+++ photo_db_cli/photo_db_cli.js
@@ -2,129 +2,120 @@
 
 const program = require('commander')
 const db = require('./db_logic')
+const fs = require('fs')
 
 program
-	.version('0.0.1')
-	.description('Query and modify database!')
+  .version('0.0.1')
+  .description('Query and modify an S3 database!')
 
 program
-	.command('addUser <username>')
-	.alias('au')
-	.description('Add user to the database')
-	.action((username) => {
-
-		db.add_bucket(username)
-		.then((data) => {
-			console.log(data)
-		})
-		.catch((err) => {
-			console.log(err)
-		})
-
-	})
-
-program
-	.command('addPhoto <username> <filepath> [filename]')
-	.alias('ap')
-	.description('Add photo to a users collection')
-	.action((username,filepath,filename) => {
-
-		filename = String(filename)|| String(filepath)
-		console.log(username,filepath,filename)
-
-		fs.readFile(filepath, (err,data) => {
-
-			db.add_object(username,filename,data)
-			.then((data) => {
-				console.log(data)
-			})
-			.catch((err) => {
-				console.log(err)
-			})
-		})
-	})
+  .command('adduser <username>')
+  .alias('au')
+  .description('add user to the database')
+  .action((username) => {
+    db.add_bucket(username)
+      .then((data) => {
+        console.log(username + " added to database.")
+      })
+      .catch((err) => {
+        console.log(username + " could not be added to database.")
+        console.log(err)
+      })
+  })
 
 program
-	.command('getUsers')
-	.alias('gu')
-	.description('Get list of users in database')
-	.action(() => {
-
-		db.get_buckets()
-		.then((data) => {
-			console.log(data)
-		})
-		.catch((err) => {
-			console.log(err)
-		})
-
-	})
-
-program
-.command('getPhotos <username> [numphotos]')
-.alias('gps')
-.description('Get list of users photos')
-.action((username,numphotos) => {
-
-	numphotos = numphotos || 1000
-
-	db.get_objects(username, numphotos)
-	.then((data) => {
-		console.log(data)
-	})
-	.catch((err) => {
-		console.log(err)
-	})
-
-})
+  .command('addphoto <username> <filepath> [filename]')
+  .alias('ap')
+  .description('add photo to a users collection')
+  .action((username, filepath, filename) => {
+    // Use file path as name if filename was not provided
+    filename = String(filename) || String(filepath)
+    fs.readFile(filepath, (err, data) => {
+      db.add_object(username, filename, data)
+        .then((data) => {
+          console.log(filename + " added to " + username + "\'s collection.")
+        })
+        .catch((err) => {
+          console.log(filename + " could not be added to " + username + "\s collection.")
+          console.log(err)
+        })
+    })
+  })
 
 program
-	.command('deleteUser <username>')
-	.alias('du')
-	.description('Delete user from the database')
-	.action((username) => {
+  .command('listusers')
+  .alias('lu')
+  .description('print list of users in database')
+  .action(() => {
+    db.list_buckets()
+      .then((data) => {
+        console.log(data)
+      })
+      .catch((err) => {
+        console.log("could not get users.")
+        console.log(err)
+      })
+  })
 
-		db.delete_bucket(username)
-		.then((data) => {
-			console.log(data)
-		})
-		.catch((err) => {
-			console.log(err)
-		})
+program
+  .command('listphotos <username> [numphotos]')
+  .alias('lp')
+  .description('print list of users photos')
+  .action((username, numphotos) => {
+    numphotos = numphotos || 1000
+    db.list_objects(username, numphotos)
+      .then((data) => {
+        console.log(data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  })
 
-	})
+program
+  .command('deleteuser <username>')
+  .alias('du')
+  .description('delete user from the database')
+  .action((username) => {
+    db.delete_bucket(username)
+      .then((data) => {
+        console.log(username + " has been deleted from the database.")
+      })
+      .catch((err) => {
+        console.log(username + " could not be deleted from the database.")
+        console.log(err)
+      })
+  })
 
 
 program
-	.command('deletePhoto <username> <photokey>')
-	.alias('dp')
-	.description('Delete photo from a users collection')
-	.action((username, photokey) => {
-
-		db.delete_object(username,photokey)
-		.then((data) => {
-			console.log(data)
-		})
-		.catch((err) => {
-			console.log(err)
-		})
-
-	})
+  .command('deletephoto <username> <photokey>')
+  .alias('dp')
+  .description('delete photo from a users collection')
+  .action((username, photokey) => {
+    db.delete_object(username, photokey)
+      .then((data) => {
+        console.log(photokey + " has been deleted from " + username + "\'s collection.")
+      })
+      .catch((err) => {
+        console.log(photokey + " could not be deleted from " + username + "\'s collection.")
+        console.log(err)
+      })
+  })
 
 program
-	.command('getPhoto <username> <photokey>')
-	.alias('gp')
-	.description('Download file to current directory')
-	.action((username, photoname) => {
-
-		db.get_object(username,photoname)
-		.then((data) => {
-			console.log(data)
-		})
-		.catch((err) => {
-			console.log(err)
-		})
-
-	})
+  .command('getphoto <username> <photokey>')
+  .alias('gp')
+  .description('download file to current directory')
+  .action((username, photoname) => {
+    db.get_object(username, photoname)
+      .then((data) => {
+        console.log(photoname + " from " + username + "\'s collection has been downloaded")
+      })
+      .catch((err) => {
+        console.log(photoname + " from " + username + "\'s collection could not be downloaded")
+        console.log(err)
+      })
+  })
 
 program.parse(process.argv)
